37b0af0ab384c3140be360d7f96be459ea6bbbf0
Santiago <santiagov801@gmail.com>
2022-10-23 15:52:52 -0500

    post details page

---
+++ components/CommentsForm.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function CommentsForm() {
+  return <div>CommentsForm</div>;
+}
--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { getPost, getPostDetails } from '../../services/index';
+import { getPost, getPostDetails, getPosts } from '../../services/index';
 import {
   PostDetail,
   Categories,
@@ -9,6 +9,46 @@ import {
   CommentsForm,
 } from '../../components';
 
-export default function postDetails() {
-  return <div>postDetails</div>;
+export default function postDetails({ post }) {
+  console.log(post);
+
+  return (
+    <div className='container mx-auto px-10 mb-8'>
+      <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
+        <div className='col-span-1 lg:col-span-8'>
+          <PostDetail post={post} />
+          <Author author={post.author} />
+          <CommentsForm slug={post.slug} />
+          <Comments slug={post.slug} />
+        </div>
+        <div className='col-span-1 lg:col-span-4'>
+          <div className='relatiev lg:sticky top-8'>
+            <PostWidget
+              slug={post.slug}
+              categories={post.categories.map((category) => {
+                category.slug;
+              })}
+            />
+            <Categories />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export async function getStaticProps({ params }) {
+  const data = await getPostDetails(params.slug);
+  return {
+    props: { post: data },
+  };
+}
+
+export async function getStaticPaths() {
+  const posts = await getPosts();
+
+  return {
+    paths: posts.map(({ node: { slug } }) => ({ params: { slug } })),
+    fallback: false,
+  };
 }
