44a35108225bc931a4fd197a1071c2767766b0df
Santiago <santiagov801@gmail.com>
2022-10-22 21:57:50 -0500

    recent posts widget / categories widget

---
+++ pages/post/[slug].js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { getPost, getPostDetails } from '../../services/index';
+import {
+  PostDetail,
+  Categories,
+  PostWidget,
+  Author,
+  Comments,
+  CommentsForm,
+} from '../../components';
+
+export default function postDetails() {
+  return <div>postDetails</div>;
+}
